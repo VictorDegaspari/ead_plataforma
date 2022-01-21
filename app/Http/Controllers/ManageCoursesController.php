4b05a5fce1a2f968248c17461f074d934e977003
Victor Degaspari <vdegaspari.vm@gmail.com>
2022-01-21 11:44:50 -0300

    fix like

--- app/Http/Controllers/ManageCoursesController.php
+++ app/Http/Controllers/ManageCoursesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Course;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class ManageCoursesController extends Controller
 {
@@ -20,9 +21,9 @@ class ManageCoursesController extends Controller
         if (is_null($course)) {
             return $this->sendError('Course not found.');
         }
-        $user = auth()->user();
+        $user = Auth::user();
 
-        $user->courses()->toggle($course->id);
+        $user->courses()->toggle([$course->id]);
 
         return response()->json([
             'message' => 'Course added to the list.',
