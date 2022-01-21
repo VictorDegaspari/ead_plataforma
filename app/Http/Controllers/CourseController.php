4de25126617753858d117514846ee81bbca5d461
Victor Degaspari <vdegaspari.vm@gmail.com>
2022-01-21 10:26:09 -0300

    fix deleted course

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -5,8 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\BaseController as BaseController;
 use App\Models\Course;
+use App\Models\User;
 use App\Models\Category;
 use Validator;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Resources\Course as CourseResource;
 
 class CourseController extends BaseController
@@ -112,9 +115,24 @@ class CourseController extends BaseController
      */
     public function destroy(Course $course)
     {
-        $course->delete();
+        $user = Auth::user();
 
-        return $this->sendResponse([], 'Course deleted successfully.');
+        $category_id = DB::table('categories_courses')
+            ->select('categories_id')
+            ->where('courses_id', $course->id)
+            ->first();
+
+        if (isset($category_id->categories_id)) {
+            $category = Category::find($category_id->categories_id);
+            $category->courses()->detach($course->id);
+        }
+
+        $user->courses()->detach($course->id);
+
+        $course->delete();
+        return $this->sendResponse([], 'Course deleted');
+
+
     }
 
 }
